a53b2ae801d5d35ec21a765c31889b302e3241f0
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-08-24 20:20:37 +0600

    Made a placeholder for now for the selected items in the shopping bag dropdown menu. Still yet to implement the functionality.

--- shoppingcart.js
+++ shoppingcart.js
@@ -4,23 +4,19 @@ cartArray = [];
 const addToCartButton = (target) => {
   const nameOfProduct =
     target.parentElement.parentElement.childNodes[1].innerText;
-  const priceOfProduct =
-    target.parentElement.childNodes[1].innerText.split(" ")[1];
+  const priceOfProduct = parseFloat(
+    target.parentElement.childNodes[1].innerText.split(" ")[1]
+  );
   const imagePathOfProduct =
     target.parentElement.parentElement.parentElement.childNodes[1].src;
 
-  // CHECKING WHETHER ITEM IS ALREADY ADDED TO CART
-  //   IF ITEM IS SELECTED, THEN ADJUST THE COUNT
-  let itemAlreadySelected = false;
-  for (item of cartArray) {
-    if (item.name === nameOfProduct) {
-      itemAlreadySelected = true;
-      item.count++;
-      break;
-    }
-  }
-  //   IF ITEM IS NOT ALREADY SELECTED, THEN ADD IT CART
-  if (!itemAlreadySelected) {
+  const existingCartItem = cartArray.find(
+    (item) => item.name === nameOfProduct
+  );
+
+  if (existingCartItem) {
+    existingCartItem.count++;
+  } else {
     cartArray.push({
       name: nameOfProduct,
       price: priceOfProduct,
@@ -28,11 +24,12 @@ const addToCartButton = (target) => {
       count: 1,
     });
   }
-  updateCartItemNot();
+  updateCartItemNotification();
+  console.log(cartArray);
 };
 
 // UPDATE SHOPPING CART NOTIFICATION COUNTER
-const updateCartItemNot = () => {
+const updateCartItemNotification = () => {
   document.getElementById("shoppingCartNotificationCounter").innerText =
     cartArray.reduce((sum, current) => sum + current.count, 0).toString();
 };
